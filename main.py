8eaffb8a176f1f68ae67ca8c301f28a95307571f
Aaron Dykes <aaronmdykes@gmail.com>
2024-09-24 16:21:16 -0400

    Added print statement to wrong letter guesses

--- main.py
+++ main.py
@@ -124,6 +124,7 @@ while (secret_word_list != list(random_word)) and (num_wrong_answers != 6):
             game_art.hangman(num_wrong_answers)   
     else:
         print('Nope! There aren\'t any {}\'s is the word.\n'.format(guessed_letter.upper()))
+        print(update_secret_word(random_word, secret_word_list, guessed_letter))
         num_wrong_answers += 1
         game_art.hangman(num_wrong_answers)
         
